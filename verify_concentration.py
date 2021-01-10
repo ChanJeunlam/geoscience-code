b83e28bee0a5b66f2df097fc6021efd07befbd4d
JiyuanGao <jiyuangao415@gmail.com>
2021-01-10 21:33:15 +0800

    update

--- verify_concentration.py
+++ verify_concentration.py
@@ -6,35 +6,54 @@ import matplotlib.pyplot as plt
 from cartopy.util import add_cyclic_point
 import cartopy.mpl.ticker as mticker
 import cartopy
+import pandas as pd
 
 # put all data into a list
-yr = ['0001'
-      #,'0002','0003','0004','0005','0006','0007','0008','0009','0010'
+yr = [
+      #'0001','0002','0003','0004','0005',
+      '0006','0007','0008','0009','0010'
       ]
-mon = ['01','02','03','04','05','06','07','08','09','10','11','12']
+mon = [
+       '01','02','03','04','05','06','07','08','09','10','11','12'
+       ]
 Base = []
 for i in range(len(yr)):
     for j in range(len(mon)):
         Base.append(xr.open_dataset
-                         ('F:/Base.cam.h0.'+yr[i]+'-'+mon[j]+'.nc'))   
+                     ('F:/Base.cam.h0.'+yr[i]+'-'+mon[j]+'.nc'))   
 AClean = []
 for i in range(len(yr)):
     for j in range(len(mon)):
         AClean.append(xr.open_dataset
-                           ('F:/AClean.cam.h0.'+yr[i]+'-'+mon[j]+'.nc'))
+                     ('F:/AClean.cam.h0.'+yr[i]+'-'+mon[j]+'.nc'))
 
 
 
 # calculate concentration differece
-var = ['bc_a1_SRF','bc_a4_SRF','pom_a1_SRF','pom_a4_SRF','so4_a1_SRF',
-       'so4_a2_SRF','so4_a3_SRF','soa_a1_SRF','soa_a2_SRF','AEROD_v']
+    #calculate concentration difference individually
+var = [
+       'bc_a1_SRF','bc_a4_SRF','pom_a1_SRF','pom_a4_SRF','so4_a1_SRF',
+       'so4_a2_SRF','so4_a3_SRF','soa_a1_SRF','soa_a2_SRF'
+       #AEROD_v
+       ]
+
 conc_diff = np.zeros((10,192,288))
 for m in range(len(var)):
     for n in range(len(Base)):
-        conc_diff[m] +=  (np.reshape(AClean[n][var[m]].data,(192,288))*np.reshape(AClean[n]['PS'].data,(192,288))/np.reshape(AClean[n]['TS'].data,(192,288))/287.05*1e9
-                          -np.reshape(Base[n][var[m]].data,(192,288))*np.reshape(Base[n]['PS'].data,(192,288))/np.reshape(Base[n]['TS'].data,(192,288))/287.05*1e9)
- 
-                  
+        conc_diff[m] +=  (
+            ( pd.DataFrame(np.reshape(AClean[n][var[m]].data,(192,288))).fillna(0) ) *np.reshape(AClean[n]['PS'].data,(192,288))/np.reshape(AClean[n]['TS'].data,(192,288))/287.05*1e9 
+           -( pd.DataFrame(np.reshape(Base[n][var[m]].data,(192,288))).fillna(0)) *np.reshape(Base[n]['PS'].data,(192,288))/np.reshape(Base[n]['TS'].data,(192,288))/287.05*1e9
+                         )
+# =============================================================================
+#     #calculate concentration difference totally
+# var_ = ['bc_SRF','pom_SRF','so4_SRF','soa_SRF']
+# conc_diff_ =[]
+# conc_diff_.append(conc_diff[0]+conc_diff[1])
+# conc_diff_.append(conc_diff[2]+conc_diff[3])
+# conc_diff_.append(conc_diff[4]+conc_diff[5]+conc_diff[6])
+# conc_diff_.append(conc_diff[7]+conc_diff[8])   
+#                 
+# =============================================================================
 # plot horizontal distribution     
 for m in range(len(var)):
     # remove middle white line
@@ -56,8 +75,8 @@ for m in range(len(var)):
     ax1.add_feature(cartopy.feature.BORDERS)
     
     # set extent
-    #extent=[70,150,15,55]
-    #ax1.set_extent(extent)
+    # extent=[-180,180,-90,80]
+    # ax1.set_extent(extent)
     
     # set lon&lat
     ax1.set_xticks([-160,-120,-80,-40,0,40,80,120,160])
@@ -87,6 +106,8 @@ for m in range(len(var)):
     cb1.ax.tick_params(labelsize=10)  
     cb1.set_label('µg/m³')
      
-    plt.savefig('F:/conc_diff_'+var[m]+'.png',  dpi=300)
+    plt.savefig('F:/conc_diff_'+var[m]+'.png',
+                dpi=300, 
+                bbox_inches = 'tight')
     plt.show()
 
